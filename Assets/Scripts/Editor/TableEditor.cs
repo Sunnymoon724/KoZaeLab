b5b3b6adff1faa9b4b9dfbf8b26653381e6e664f
Sunnymoon <sunnymoon724@gmail.com>
2021-08-17 19:35:39 +0900

    구글 시트 개발 중

---
+++ Assets/Scripts/Editor/TableEditor.cs
@@ -0,0 +1,47 @@
+using Sirenix.OdinInspector.Editor;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+public partial class TableEditor : OdinMenuEditorWindow
+{
+    partial void AddOdinMenuTree(ref OdinMenuTree _tree)
+    {
+        if(Directory.Exists(SheetSetting.In.ScriptPath))
+        {
+            foreach(var path in Directory.GetFiles(SheetSetting.In.ScriptPath,"*.cs"))
+            {
+                var name = Path.GetFileNameWithoutExtension(path);
+                var type = System.Type.GetType($"Table.{name}, Assembly-CSharp");
+
+                if(type != null)
+                {
+                    var guid = AssetDatabase.FindAssets($"t:{name} {name}").FirstOrDefault();
+                    var data = string.IsNullOrEmpty(guid) ? CreateTable(name,type) : AssetDatabase.LoadAssetAtPath(AssetDatabase.GUIDToAssetPath(guid),type);
+
+                    if(data != null)
+                    {
+                        _tree.Add(name,data);
+                    }
+                }
+            }
+
+            AssetDatabase.Refresh();
+        }
+    }
+
+    Object CreateTable(string _name,System.Type _type)
+    {
+        var path = Path.Combine(SheetSetting.In.AssetPath,string.Concat(_name,".asset"));
+        int index = path.IndexOf("Assets");
+        var instance = CreateInstance(_type);
+
+        if(instance != null)
+        {
+            AssetDatabase.CreateAsset(instance,path.Substring(index));
+        }
+
+        return instance;
+    }
+}
