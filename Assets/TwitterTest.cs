93223cd38248dc6b5f711316bc05a77285ff58cd
KoZae <sunnymoon724@gmail.com>
2023-08-03 02:24:33 +0900

    웹뷰로 트위터 로그인 만드는 중

---
+++ Assets/TwitterTest.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using Twity;
+using Twity.Helpers;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class TwitterTest : MonoBehaviour
+{
+	[SerializeField]
+	private Button m_Button;
+
+	[SerializeField]
+	private UniWebView m_UniWebView;
+
+	[SerializeField]
+	private Text m_Text;
+
+	private void Awake()
+	{
+		m_Button.SetOnClickListener(()=>
+		{
+			StartCoroutine(Twity.Client.GenerateRequestToken((result)=>
+			{
+				if(result)
+				{
+					StartCoroutine(GetLogin((result)=>
+					{
+						Debug.Log("end");
+
+					}));
+				}
+			}));
+		});
+	}
+
+	public IEnumerator GetLogin(Action<bool> callback)
+	{
+		// Debug.Log(Oauth.authorizeURL+"?force_login=true");
+
+		UnityWebRequest request = UnityWebRequest.Get(Oauth.authorizeURL);
+		// request.SetRequestHeader("Authorization","");
+
+		yield return request.SendWebRequest();
+
+		
+		Debug.Log(request.responseCode);
+		// Debug.Log(request.error);
+
+		if (request.isNetworkError) callback(false);
+
+		if (request.responseCode == 200 || request.responseCode == 201)
+		{
+			m_Text.text = request.downloadHandler.text;
+
+			Debug.Log(request.downloadHandler.text);
+
+			m_UniWebView.LoadHTMLString(request.downloadHandler.text,null);
+
+			m_UniWebView.OnPageFinished += (view, statusCode, url) =>
+			{
+				m_Text.text = "code :" +statusCode+" url : "+url;
+			};
+
+			m_UniWebView.OnMessageReceived += (view, message) =>
+			{
+				m_Text.text = message.RawMessage;
+			};
+
+			m_UniWebView.OnPageErrorReceived  += (view, errorCode, message) =>
+			{
+				m_Text.text = $"Error Code: {errorCode}, Message: {message}";
+			};
+
+			m_UniWebView.Show();
+
+			callback(true);
+		}
+		else
+		{
+			callback(false);
+		}
+	}
+}
