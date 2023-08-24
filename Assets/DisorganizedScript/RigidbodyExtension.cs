87e80b94eecc1e15e8ddd245b0034776ca27f453
kozae-mac <sunnymoon724@gmail.com>
2023-08-25 07:54:36 +0900

    코드 정리1

--- Assets/DisorganizedScript/RigidbodyExtension.cs
+++ Assets/DisorganizedScript/RigidbodyExtension.cs
@@ -5,6 +5,6 @@ public static class RigidbodyExtension
 {
 	public static void ChangeDirection(this Rigidbody _rigid,Vector3 _direction)
 	{
-		_rigid.velocity = _direction * _rigid.velocity.magnitude;
+		_rigid.velocity = _direction*_rigid.velocity.magnitude;
 	}
 }
