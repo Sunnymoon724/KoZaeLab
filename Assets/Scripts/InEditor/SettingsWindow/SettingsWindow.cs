7486c83811ec5b3df934ac2285b9a6accd618fe3
KoZae <sunnymoon724@gmail.com>
2023-09-05 01:01:13 +0900

    네트워크 추가

--- Assets/Scripts/InEditor/SettingsWindow/SettingsWindow.cs
+++ Assets/Scripts/InEditor/SettingsWindow/SettingsWindow.cs
@@ -12,6 +12,7 @@ namespace KZLib.Window
 			_tree.Add("언어 설정",LanguageSettings.In);
 			_tree.Add("메타 설정",MetaSettings.In);
 			_tree.Add("빌드 설정",BuildSettings.In);
+			_tree.Add("통신 설정",NetworkSettings.In);
 		}
 	}
 }
