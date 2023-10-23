56cab3e44e34412f6e8638fc663443ebf40eec8a
KoZae <sunnymoon724@gmail.com>
2023-10-24 02:31:30 +0900

    스크롤 렉트 샘플 추가

---
+++ Assets/1.Samples/UISample/ScrollRect/Scripts/SampleSlot.cs
@@ -0,0 +1,13 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace KZLib.KZSample
+{
+	public class SampleSlot : UISlot
+	{
+		protected override bool UseImage => false;
+
+		protected override bool UseButton => false;
+	}
+}
