4f394f25245f406cc64237b3580043e732a56672
KoZae <sunnymoon724@gmail.com>
2023-11-20 03:02:32 +0900

    뷰어 정리

--- Assets/1.Samples/Scene/ScrollRect/Scripts/ScrollRectCon.cs
+++ Assets/1.Samples/Scene/ScrollRect/Scripts/ScrollRectCon.cs
@@ -7,26 +7,107 @@ namespace KZLib.KZSample
 {
 	public class ScrollRectCon : BaseComponent
 	{
-		public class SampleData : CellData
+		private class SampleData : CellData
 		{
 			public SampleData(int _index) : base(string.Format("{0}",_index),null,null,null) { }
 		}
 
-		[SerializeField] private UIScrollRect m_VerticalScrollRect = null;
+		[VerticalGroup("0",Order = 0),SerializeField]
+		private UIScrollRect m_VerticalScrollRect = null;
 
-		[SerializeField] private UIScrollRect m_HorizontalScrollRect = null;
+		[VerticalGroup("0",Order = 0),SerializeField]
+		private UIScrollRect m_HorizontalScrollRect = null;
 
-		[ShowInInspector,ReadOnly] private int Count => m_CellList.Count;
+		[SerializeField,HideInInspector]
+		private int m_CellCount = 10;
 
-		[SerializeField] private int m_CellCount = 10;
+		[VerticalGroup("1",Order = 1),ShowInInspector]
+		private int CellCount
+		{
+			get => m_CellCount;
+			set
+			{
+				if(m_CellCount == value)
+				{
+					return;
+				}
 
-		private List<ICellData> m_CellList = new();
+				m_CellCount = value;
+
+				if(Application.isPlaying)
+				{
+					SetCellList();
+				}
+			}
+		}
+
+		[VerticalGroup("2",Order = 2),SerializeField,Range(0.0f,1.0f)]
+		private float m_Duration = 0.5f;
+		[VerticalGroup("2",Order = 2),SerializeField,ValueDropdown("m_OrderList")]
+		private int m_Order = 0;
+
+		private readonly List<int> m_OrderList = new();
+
+		[HorizontalGroup("3",Order = 3),Button("Move To Top")]
+		private void OnMoveToTop()
+		{
+			if(m_VerticalScrollRect.gameObject.activeInHierarchy)
+			{
+				m_VerticalScrollRect.MoveToTop(m_Order,m_Duration);
+			}
+
+			if(m_HorizontalScrollRect.gameObject.activeInHierarchy)
+			{
+				m_HorizontalScrollRect.MoveToTop(m_Order,m_Duration);
+			}
+		}
+
+		[HorizontalGroup("3",Order = 3),Button("Move To Center")]
+		private void OnMoveToCenter()
+		{
+			if(m_VerticalScrollRect.gameObject.activeInHierarchy)
+			{
+				m_VerticalScrollRect.MoveToCenter(m_Order,m_Duration);
+			}
+
+			if(m_HorizontalScrollRect.gameObject.activeInHierarchy)
+			{
+				m_HorizontalScrollRect.MoveToCenter(m_Order,m_Duration);
+			}
+		}
+
+		[HorizontalGroup("3",Order = 3),Button("Move To Bottom")]
+		private void OnMoveToBottom()
+		{
+			if(m_VerticalScrollRect.gameObject.activeInHierarchy)
+			{
+				m_VerticalScrollRect.MoveToBottom(m_Order,m_Duration);
+			}
+
+			if(m_HorizontalScrollRect.gameObject.activeInHierarchy)
+			{
+				m_HorizontalScrollRect.MoveToBottom(m_Order,m_Duration);
+			}
+		}
+
+		private readonly List<ICellData> m_CellList = new();
 
 		private void Start()
 		{
-			for(var i=0;i<m_CellCount;i++)
+			SetCellList();
+		}
+
+		private void SetCellList()
+		{
+			var count = CellCount;
+
+			m_CellList.Clear();
+			m_OrderList.Clear();
+
+			for(var i=0;i<count;i++)
 			{
 				m_CellList.Add(new SampleData(i));
+				m_OrderList.Add(i);
 			}
 
 			if(m_VerticalScrollRect.gameObject.activeInHierarchy)
