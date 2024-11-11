c848bcbeaded818daa089d60206ab2ca0e4df2a1
KoZae <sunnymoon724@gmail.com>
2024-11-11 22:54:23 +0900

    연구소 이전

---
+++ KZMainLab/Assets/Test.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using Cysharp.Threading.Tasks;
+using KZLib;
+using KZLib.KZSchedule;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public delegate void Action2();
+
+public class Test : MonoBehaviour
+{
+    
+    private void Start()
+    {
+        List<int> ww = new List<int>();
+
+
+        ww.Add(0);
+    }
+}
