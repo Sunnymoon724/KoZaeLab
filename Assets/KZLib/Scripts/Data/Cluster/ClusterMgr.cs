6632b59fb566877e692060fc9fb56bb07664a61c
revolca_desktop <>
2025-07-30 00:33:31 +0900

    클러스터 수정

--- Assets/KZLib/Scripts/Data/Cluster/ClusterMgr.cs
+++ Assets/KZLib/Scripts/Data/Cluster/ClusterMgr.cs
@@ -6,10 +6,56 @@ namespace KZLib.KZData
 {
 	public class ClusterMgr : Singleton<ClusterMgr>
 	{
+        private class ClusterKey : IEquatable<ClusterKey>
+		{
+			private readonly Type m_type;
+			private readonly object[] m_paramArray;
+
+			public ClusterKey(Type type,object[] paramArray)
+			{
+				m_type = type;
+				m_paramArray = paramArray;
+			}
+
+			public bool Equals(ClusterKey other)
+			{
+				if(other == null || m_type != other.m_type || m_paramArray.Length != other.m_paramArray.Length)
+				{
+					return false;
+				}
+
+				for(var i=0;i<m_paramArray.Length;i++)
+				{
+					if(!Equals(m_paramArray[i],other.m_paramArray[i]))
+					{
+						return false;
+					}
+				}
+
+				return true;
+			}
+
+			public override bool Equals(object obj) => Equals(obj as ClusterKey);
+
+			public override int GetHashCode()
+			{
+				int hashCode = m_type.GetHashCode();
+
+				foreach (var param in m_paramArray)
+				{
+					hashCode = HashCode.Combine(hashCode,param?.GetHashCode() ?? 0);
+				}
+
+				return hashCode;
+			}
+		}
+
+
+		
 		private bool m_disposed = false;
 
 		//? Type / Cluster
-		private readonly Dictionary<string,ICluster> m_clusterDict = new();
+		private readonly Dictionary<ClusterKey,ICluster> m_clusterDict = new();
 
 		protected override void Release(bool disposing)
 		{
@@ -28,18 +74,18 @@ namespace KZLib.KZData
 			base.Release(disposing);
 		}
 
-		public TCluster GetOrCreateCluster<TCluster>(object param) where TCluster : class,ICluster
+		public TCluster GetOrCreateCluster<TCluster>(params object[] paramArray) where TCluster : class,ICluster
 		{
-			return GetOrCreateCluster(typeof(TCluster),param) as TCluster;
+			return GetOrCreateCluster(typeof(TCluster),paramArray) as TCluster;
 		}
 
-		public ICluster GetOrCreateCluster(Type type,object param)
+		public ICluster GetOrCreateCluster(Type type,params object[] paramArray)
 		{
-			var key = CryptoUtility.SHA.ComputeHashToString(param.ToString());
+			var key = new ClusterKey(type,paramArray);
 
 			if(!m_clusterDict.TryGetValue(key,out var cluster))
 			{
-				cluster = Activator.CreateInstance(type,param) as ICluster;
+				cluster = Activator.CreateInstance(type,paramArray) as ICluster;
 
 				if(cluster == null)
 				{
