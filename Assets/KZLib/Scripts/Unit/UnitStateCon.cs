2b506c3aefc5fa05d9ad167c0f2745dbecbd5700
revolca_desktop <>
2025-10-12 06:08:17 +0900

    유닛 수정

--- Assets/KZLib/Scripts/Unit/UnitStateCon.cs
+++ Assets/KZLib/Scripts/Unit/UnitStateCon.cs
@@ -4,85 +4,88 @@ using System.Threading;
 using Cysharp.Threading.Tasks;
 using Sirenix.OdinInspector;
 
-public interface IUnitStateParam { }
-
-public abstract class UnitStateCon<TEnum> : SerializedMonoBehaviour where TEnum : struct,Enum
+namespace KZLib
 {
-	protected bool m_changeAllowed = false;
-	protected CancellationTokenSource m_tokenSource = null;
-	private readonly Dictionary<TEnum,Func<CancellationToken,IUnitStateParam,UniTask<TEnum>>> m_stateFuncDict = new();
+    public interface IUnitStateParam { }
 
-	[ShowInInspector] public TEnum StateType => m_stateType;
-
-	protected TEnum m_stateType = default;
-
-	public event Action<TEnum,TEnum> OnUnitStateChanged = null;
-
-	protected abstract bool _CanChange(TEnum newStateTag,bool isForce);
-
-	public void Initialize()
+	public abstract class UnitStateCon<TEnum> : SerializedMonoBehaviour where TEnum : struct,Enum
 	{
-		m_stateFuncDict.Clear();
-	}
+		protected bool m_changeAllowed = false;
+		protected CancellationTokenSource m_tokenSource = null;
+		private readonly Dictionary<TEnum,Func<CancellationToken,IUnitStateParam,UniTask<TEnum>>> m_stateFuncDict = new();
 
-	public void Release()
-	{
-		m_stateFuncDict.Clear();
+		[ShowInInspector] public TEnum StateType => m_stateType;
 
-		CommonUtility.KillTokenSource(ref m_tokenSource);
-	}
+		protected TEnum m_stateType = default;
 
-	public async UniTask EnterStateAsync(TEnum newState,IUnitStateParam param,bool isForce = false)
-	{
-		if(isActiveAndEnabled == false || !_CanChange(newState,isForce))
+		public event Action<TEnum,TEnum> OnUnitStateChanged = null;
+
+		protected abstract bool _CanChange(TEnum newStateTag,bool isForce);
+
+		public void Initialize()
 		{
-			return;
+			m_stateFuncDict.Clear();
 		}
 
-		var curState = newState;
-
-		CommonUtility.RecycleTokenSource(ref m_tokenSource);
-
-		while(isActiveAndEnabled)
+		public void Release()
 		{
-			if(!m_stateFuncDict.TryGetValue(curState, out var stateFunc))
-			{
-				LogSvc.System.E($"{curState} state not found");
+			m_stateFuncDict.Clear();
 
+			CommonUtility.KillTokenSource(ref m_tokenSource);
+		}
+
+		public async UniTask EnterStateAsync(TEnum newState,IUnitStateParam param,bool isForce = false)
+		{
+			if(isActiveAndEnabled == false || !_CanChange(newState,isForce))
+			{
 				return;
 			}
 
-			OnUnitStateChanged?.Invoke(m_stateType,curState);
+			var curState = newState;
 
-			m_stateType = curState;
+			CommonUtility.RecycleTokenSource(ref m_tokenSource);
 
-			_ReadyState();
-
-			LogSvc.System.I($"{name} is entered {curState}");
-
-			var nextState = await stateFunc.Invoke(m_tokenSource.Token,param);
-
-			if(!_CanChange(nextState,false))
+			while(isActiveAndEnabled)
 			{
-				// wait one frame
-				await UniTask.Yield();
-			}
-			else
-			{
-				curState = nextState;
+				if(!m_stateFuncDict.TryGetValue(curState, out var stateFunc))
+				{
+					LogSvc.System.E($"{curState} state not found");
+
+					return;
+				}
+
+				OnUnitStateChanged?.Invoke(m_stateType,curState);
+
+				m_stateType = curState;
+
+				_ReadyState();
+
+				LogSvc.System.I($"{name} is entered {curState}");
+
+				var nextState = await stateFunc.Invoke(m_tokenSource.Token,param);
+
+				if(!_CanChange(nextState,false))
+				{
+					// wait one frame
+					await UniTask.Yield();
+				}
+				else
+				{
+					curState = nextState;
+				}
 			}
 		}
-	}
 
-	protected void _RegisterState(TEnum type,Func<CancellationToken,IUnitStateParam,UniTask<TEnum>> stateFunc)
-	{
-		if(stateFunc == null)
+		protected void _RegisterState(TEnum type,Func<CancellationToken,IUnitStateParam,UniTask<TEnum>> stateFunc)
 		{
-			return;
+			if(stateFunc == null)
+			{
+				return;
+			}
+
+			m_stateFuncDict[type] = stateFunc;
 		}
 
-		m_stateFuncDict[type] = stateFunc;
+		protected virtual void _ReadyState() { }
 	}
-
-	protected virtual void _ReadyState() { }
 }
